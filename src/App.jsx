a526369d1011276921e2e4bc92b445fc7eb6979b
Micah Gooding <micahgo425@gmail.com>
2025-12-03 20:05:04 -0500

    Removed student, navBar adjustments, CSS tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Footer from "./Footer.jsx";
 import Card from "./Card.jsx";
 import Button from "./Buttton.jsx";
-import Students from "./Students.jsx";
 import NavBar from "./NavBar.jsx";
 import pfp from './assets/pfp.png'
 import thanks from './assets/thanks.png'
@@ -11,9 +10,8 @@ function App() {
   return(
     <>
       <Card name='Apothecary' pfp={pfp}/>
-      <Card name='Apothecary' pfp={thanks}/>
+      <Card name='N0t_a_nam3' pfp={thanks}/>
       <NavBar/>
-      <Students name='Spongbob' age={30} isStudent={true}/>
       <Button/>
       <Footer/>
     </>
